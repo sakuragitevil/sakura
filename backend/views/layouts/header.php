2e0bf46234e70624177d7cf2b7603b87bf825b9b
sakuragitevil <sakuragitevil@gmail.com>
2016-04-05 15:19:07 +0700

    add new icon

---
+++ backend/views/layouts/header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thuan
+ * Date: 4/4/2016
+ * Time: 10:57 AM
+ */
+
+?>
+
+<div class="row-fluid sakura-header-bar">
+    <div class="col-sm-12 col-md-12 col-lg-12 noLRpadding">
+        <div class="row-fluid">
+            <div class="col-sm-5 col-md-5 col-lg-5 noLRpadding">
+                <div class="div-inline" style="min-width: 24px">
+                    <content class="xjKiLb">
+                    <span style="top: 16px">
+                        <svg width="24px" height="24px" class="pTh3n">
+                            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+                        </svg>
+                    </span>
+                    </content>
+                </div>
+                <div class="div-inline pl30" style="min-width: 120px">
+                    <content class="xjKiLb">
+                    <span style="top: 11px">
+                        <span class="form-title">Sakura Inc</span>
+                    </span>
+                    </content>
+                </div>
+                <div class="clearboth"></div>
+            </div>
+            <div class="col-sm-7 col-md-7 col-lg-7 noLRpadding">
+                <div class="div-inline">
+                    <content class="xjKiLb">
+                    <span style="top: 11px">
+                        <span class="user-title">Sakura Inc</span>
+                    </span>
+                    </content>
+                </div>
+                <div class="div-inline pl30">
+                    <content class="xjKiLb">
+                    <span style="top: 15px">
+                        <i class="glyphicon glyphicon-th symbols" aria-hidden="true"></i>
+                    </span>
+                    </content></div>
+                <div class="div-inline">
+                    <content class="xjKiLb">
+                    <span style="top: 11px">
+                        <span class="form-title">Sakura Inc</span>
+                    </span>
+                    </content>
+                </div>
+                <div class="div-inline">
+                    <content class="xjKiLb">
+                    <span style="top: 11px">
+                        <div class="ac-circle-mask">
+                            <canvas class="circle" width="32" height="32">
+
+                            </canvas>
+                        </div>
+                    </span>
+                    </content>
+                </div>
+                <div class="clearboth"></div>
+            </div>
+        </div>
+    </div>
+</div>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -20,14 +20,27 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link href="../web/img/sakura.ico" rel="shortcut icon" type="image/x-icon"/>
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <div class="container">
+    <div class="g-Ue-ad">
+        <div class="g-Qx">
+            <div class="g-Qx-eb X5yjGb">
+                <div class="kFx1Ae-xdwExf-eb-m">
+                    <img id=":0.la" class="kFx1Ae-xdwExf-eb"
+                         src="https://lh3.googleusercontent.com/4zsirp7PvHet1HV-6JWjLsTbzNkAFOJo9coBYcLXgt5WK7A-2zo3ShS4zDs-OKElEDgsgv_IdJNWRmk=w1920-h1080-p-k-nd-no">
+                </div>
+            </div>
+            <div class="g-Qx-n5VRYe"></div>
+        </div>
+    </div>
+    <div class="container-fluid">
         <?= Alert::widget() ?>
+        <?= $this->render('@app/views/layouts/header') ?>
         <?= $content ?>
     </div>
 </div>
