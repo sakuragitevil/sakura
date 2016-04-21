c1fb2936dbf74e05b2e2e93b2299774f050bf710
sakuragitevil <sakuragitevil@gmail.com>
2016-04-21 16:22:27 +0700

    Thuan-20160421-4:22PM

--- common/widgets/sakura/views/xhr2.php
+++ common/widgets/sakura/views/xhr2.php
@@ -9,12 +9,30 @@
                 <div class="xhr2-hn-Pn-eb">
                     <div class="xhr2-hn-Pn-hc xhr2-hn-On-Zb-Nd">
                         <div class="xhr2-hn-On-Zb-li-Yb-en">
-                            <div class="xhr2-a-li-Yb">
-                                <div class="xhr2-a-li xhr2-a-li-w">
+                            <div class="xhr2-a-li-Yb"  role="tablist" id="xhr2tab">
+                                <div class="xhr2-a-li xhr2-a-li-w" role="tab">
                                     <div class="xhr2-hn-On-Zb-bp">Upload photos</div>
                                 </div>
                                 <div class="xhr2-a-li">
-                                    <div class="xhr2-hn-On-Zb-bp">Your photos</div>
+                                    <div class="xhr2-hn-On-Zb-bp" role="tab">Your photos</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="xhr2-hn-On-Zb-eb">
+                        <div class="xhr2-hn-Rn-Qn">
+                            <div class="xhr2-hn-eb">
+                                <div class="xhr2-hn-Vt">
+                                    <div class="xhr2-hn-Vt-nu">
+                                        <div class="xhr2-hn-Eo-vq-wk-dm">
+                                            <span class="glyphicon glyphicon-picture"></span>
+                                        </div>
+                                        <div class="xhr2-hn-Vt-tc">Drag a profile photo here</div>
+                                        <div class="xhr2-hn-Vt-qu">
+                                            <div class="xhr2-hn-Vt-ru">— or —</div>
+                                        </div>
+                                        <button type="button" class="btn btn-default btn-sm">Select a photo from your computer</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -22,8 +40,8 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Set as profile photo</button>
+                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary btn-sm disabled">Set as profile photo</button>
             </div>
         </div>
     </div>
